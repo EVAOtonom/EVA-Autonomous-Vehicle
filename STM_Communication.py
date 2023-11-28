1c9ea04a538521e05d2d1be61d20dbf60dcf77d0
Yakup OĞUZ <95951188+1yakupoguz@users.noreply.github.com>
2023-11-29 01:20:35 +0300

    Update STM_Communication.py
    Negatif değer gönderilebilir.

--- STM_Communication.py
+++ STM_Communication.py
@@ -1,11 +1,27 @@
 import minimalmodbus
-#slaveadress = 1 -> STM'in kaçıncı slave olduğunu belirtir.
+
+#32767 ile -32768 aralığında değer gönderilebilir. Fonksiyon içerisine 32767 ila -32768 aralığında değer giriniz.
+
+#slaveadress = 1 ->     STM'in kaçıncı slave olduğunu belirtir. Tek STM kullandığımız için 1 olmalı.
+#functioncode=6  ->     Write Single Register
 #register_adress = 0 -> Direksiyon açısı komutu göndermeyi sağlar.
-instrument = minimalmodbus.Instrument('COM4',slaveaddress=1) 
+#register_adress = 1 -> Fren komutu göndermeyi sağlar. 1 frene basar 0 freni salar.
+#register_adress = 2 -> Motora komut göndermeyi sağlar.
+
+instrument = minimalmodbus.Instrument('COM7',slaveaddress=1) 
 instrument.serial.baudrate = 38400
 
 def send_to_STM(register_adress,data):
-    write_register_address=register_adress
-    value=data
-    instrument.write_register(write_register_address, value, functioncode=6)  # Function code 6: Write Single Register
-    print(f'Register {write_register_address} değeri {data} olarak yazıldı.')
+    if data>-32769 and data<32768:
+        write_register_address=register_adress
+        if (data<0):
+            data = 65536 + data
+        else:
+            pass
+        value=data
+        instrument.write_register(write_register_address, value, functioncode=6)
+        print(f'Register {write_register_address} değeri {data} olarak yazıldı.')
+    else:
+        print("Fonksiyon içerisine 32767 ila -32768 aralığında değer giriniz.")
+
+send_to_STM(0,1000)
