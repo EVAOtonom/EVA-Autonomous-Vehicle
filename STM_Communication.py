cdb909ef26fb86fa863ef0167fd7b3bcce611dc2
Yakup OĞUZ <95951188+1yakupoguz@users.noreply.github.com>
2024-01-21 16:43:01 +0300

    Update STM_Communication.py
    edited

--- STM_Communication.py
+++ STM_Communication.py
@@ -1,23 +1,23 @@
 import minimalmodbus
 
-#32767 ile -32768 aralığında değer gönderilebilir. Fonksiyon içerisine 32767 ila -32768 aralığında değer giriniz.
+# BU KOD YAPAY ZEKA İŞLEMLERİ SONUCUNDA ARACIN KONTROL EDİLEBİLMESİ İÇİN YAZILMIŞTIR.
+# ARAÇ KONTROL SİSTEMİNE BAĞLI OLAN STM KONTROLCÜSÜNE BELİRLİ KOMUTLAR GÖNDEREREK ARACI HAREKET ETTİRMEYİ SAĞLAR.
 
-#slaveadress = 1 ->     STM'in kaçıncı slave olduğunu belirtir. Tek STM kullandığımız için 1 olmalı.
-#functioncode=6  ->     Write Single Register
-
-#register_adress = 0 -> Direksiyon açısı komutu göndermeyi sağlar.
-#register_adress = 1 -> Fren komutu göndermeyi sağlar. 1 frene basar 0 freni salar.
-#register_adress = 2 -> Motora komut göndermeyi sağlar.
+#register_adress = 0 -> Araca direksiyon açısı vermeyi sağlar. -40 ile 40 aralığında değer göndermelisiniz.
+#register_adress = 1 -> Araca fren yaptırmayı sağlar. 1 gönderirseniz fren yapar, 0 gönderirseniz freni bırakır.
+#register_adress = 2 -> Araca motor gücü vermeyi sağlar. 2 ile 10 aralığında değer göndermelisiniz. Genel olarak 2 yeterli.
 #register_adress = 3 -> Anlık tekerlek açısını okumayı sağlar.
 #register_adress = 4 -> Fren durumunun basılı olduğunu okumayı sağlar.
 #register_adress = 5 -> Fren durumunun basılı olmadığını okumayı sağlar.
-#register_adress = 6 -> Geri vites komutu göndermeyi sağlar.
-#register_adress = 7 -> Sola sinyal vermeyi sağlar.
-#register_adress = 8 -> Sağa sinyal vermeyi sağlar.
-#register_adress = 9 -> Acil stop yapmayı sağlar. 
-#register_adress = 10 -> Arka tekerdeki enkoderden değer okumayı sağlar. 
+#register_adress = 6 -> Gidilen mesafenin öğrenilmesini sağlar. (Arka enkoder aracılığıyla)
+#register_adress = 7 -> Geri vites komutu göndermeyi sağlar. 1 Göndermelisiniz.
+#register_adress = 8 -> Sola sinyal vermeyi sağlar. 1 Göndermelisiniz.
+#register_adress = 9 -> Sağa sinyal vermeyi sağlar. 1 Göndermelisiniz.
+#register_adress = 10 -> Acil stop yapmayı sağlar.  1 Göndermelisiniz.
+#register_adress = 11 -> Ön farların açılmasını sağlar. 1 Göndermelisiniz.
+#register_adress = 12 -> Aracın manuel sürüş moduna geçmesini sağlar.
 
-instrument = minimalmodbus.Instrument('COM7',slaveaddress=1) 
+instrument = minimalmodbus.Instrument('COM10',slaveaddress=1) 
 instrument.serial.baudrate = 38400
 
 def send_to_STM(register_adress,data):
@@ -36,7 +36,8 @@ def send_to_STM(register_adress,data):
     except minimalmodbus.ModbusException as e:
         print(f"hata : {e}")
         pass
-        
+
 if __name__ == "__main__":
-    send_to_STM(0,40)
+
+    send_to_STM(0,20)
     #instrument.read_register(10)
