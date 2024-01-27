bff8df29aa0e3e6e54bff702f3bd99344f38ebb0
Sefa-Kara <119665064+Sefa-Kara@users.noreply.github.com>
2024-01-27 22:58:00 +0300

    aks_communication.py

--- Camera_node/aks_communication.py
+++ Camera_node/aks_communication.py
@@ -2,11 +2,11 @@ import serial
 import time
 
 #AKS Kartı tanımlanıyor. Burada timeout değeri arduinodan gelen veri 0.1 saniyede gelmezse iletişimi kesmesi içindir.
-#arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
-#time.sleep(2)
+arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
+time.sleep(2)
 
 def sendToArduino(x):
-    #arduino.write(x.encode())
+    arduino.write(x.encode())
     time.sleep(0.1)
     #Seri haberleşmede gönderilecek veriyi uygun formata çevirmek gerekir.
     #Bunun için encode() fonksiyonu kullanılır. Bu sayede 8 bitlik dizilere (UTF-8) çevrilir. 
