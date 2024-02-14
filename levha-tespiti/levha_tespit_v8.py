51115e3be4c53d91287a0f2b910db1ec526e7313
Yakup OĞUZ <95951188+1yakupoguz@users.noreply.github.com>
2024-02-14 17:22:17 +0300

    Update levha_tespit_v8.py
    14.02 update

--- levha-tespiti/levha_tespit_v8.py
+++ levha-tespiti/levha_tespit_v8.py
@@ -1,11 +1,50 @@
-#!/usr/bin/env python3.9
 import time
 from ultralytics import YOLO
 import cv2
+import math
 import AKS_Communication as aks
-
 stm = aks.STM_Communication("/dev/ttyUSB0")
-model=YOLO('/home/otonom/otonom_ws/src/levha_tespiti/src/29.01best.pt')
+model=YOLO('/home/otonom/Desktop/otonom-git/EVA-Autonomous-Vehicle/levha-tespiti/29.01best.pt')
+
+class_names = ['20', '30', 'dur', 'durak', 'girisyok', 'ilerisag', 'ilerisol', 'kirmizi', 'park', 'parkyasak', 'sag', 'sagadonulmez', 'sari', 'sol', 'soladonulmez', 'yesil', 'engellipark', 'tasitrafiginekapali', 'yayagecidi', 'kavsak', 'ikiliyon', 'tersengellipark','parkYapilmaz']
+    
+def object_detection(device):
+    cap = cv2.VideoCapture(device)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    frame_count = 0
+    detection_interval = 4
+    while True:
+        if not cap.isOpened():
+            print("Kamera açılamadı!")
+            break
+        success, frame = cap.read()
+        if not success:
+            print("frame okunamadı!")
+            break
+        frame_count += 1
+        if frame_count % detection_interval == 0:
+            results = model(frame, stream=True)
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    # put box in cam
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+                    confidence = math.ceil((box.conf[0]*100))/100
+                    detected_sign = class_names[int(box.cls[0])]
+                    
+                    cv2.putText(frame, detected_sign+" "+str(confidence), [x1, y1], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+
+        cv2.imshow('EVA Object Detection', frame)
+        if cv2.waitKey(1) == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 def hareket_durak():
         
@@ -15,7 +54,6 @@ def hareket_durak():
         time.sleep(2) 
         stm.send_command(aks.Register.MOTOR_POWER,2)        
 
-                
         while stm.read_data(aks.Register.READ_ODOMETER) < 250:
             continue 
         
@@ -82,30 +120,8 @@ def hareket_sag():
             continue 
 
         stm.send_command(aks.Register.RESET_ENCODER,1)        
-        stm.send_command(aks.Register.MOTOR_POWER,0) 
-
-def camera():
-    cap = cv2.VideoCapture(2)
-    
-    while cap.isOpened() :
-        ret,frame = cap.read()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-        results=model(frame)
-
-        for r in results:
-            a=r.boxes
-            b = a.cls.cpu()
-            c=b.numpy()
-            if c == 3 :
-                hareket_durak()
-            elif c == 10 :
-                hareket_sag()
-
-    cap.release()
-    cv2.destroyAllWindows()
-
+        stm.send_command(aks.Register.MOTOR_POWER,0)
+        
 stm.send_command(aks.Register.RESET_ENCODER,1)
 stm.send_command(aks.Register.MOTOR_POWER,2)
-camera()
+object_detection(0)
