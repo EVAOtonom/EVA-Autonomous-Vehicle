b867cf4bd3c7141ba8b94c0dcd1f896e7efe2917
Yakup OĞUZ <95951188+1yakupoguz@users.noreply.github.com>
2024-02-26 12:40:33 +0300

    Update RPLidarS1.py
    added lazer_gucu condition

--- engel-tespiti/RPLidarS1.py
+++ engel-tespiti/RPLidarS1.py
@@ -1,7 +1,7 @@
 import time
 from rplidar import RPLidar
 
-PORT_NAME = 'COM3'
+PORT_NAME = '/dev/ttyUSB0'
 distances = [0]
 lidar = RPLidar(PORT_NAME, baudrate=256000, timeout=2)
 sayac=0
@@ -9,10 +9,11 @@ sayac=0
 try:
     time.sleep(0.5)
     for bool, lazer_gucu, angle, distance in lidar.iter_measurments(max_buf_meas=500):
-        if 360 > angle > 350 or 10>angle>0:             #TESTTE KONTROL ET              #ARACIN ORTA NOKTASINI TARAR
-            sayac+=1
-            print(distance,sayac)
-            
+        if lazer_gucu>2:
+            if 360 > angle > 350 or 10>angle>0:             #TESTTE KONTROL ET              #ARACIN ORTA NOKTASINI TARAR
+                sayac+=1
+                print(bool,lazer_gucu,distance,sayac)
+                
 except:
     print("EXCEPT ÇALISTI LİDAR BAŞLAMADI...")
     lidar.stop()
