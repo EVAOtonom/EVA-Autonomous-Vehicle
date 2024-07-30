f62e04c439e7fb4a142ca819ccd7f2792b696e61
Akifdlk <86167391+akif-dlk@users.noreply.github.com>
2024-07-31 00:53:22 +0600

    AKS_Communication.py topic olacak şekilde güncellendi

--- AKS_Communication.py
+++ AKS_Communication.py
@@ -1,17 +1,11 @@
 
 # BU KOD YAPAY ZEKA İŞLEMLERİ SONUCUNDA ARACIN KONTROL EDİLEBİLMESİ İÇİN YAZILMIŞTIR.
-# ARAÇ KONTROL SİSTEMİNE BAĞLI OLAN STM KONTROLCÜSÜNE BELİRLİ KOMUTLAR GÖNDEREREK ARACI HAREKET ETTİRMEYİ SAĞLAR.
+# gps_latitude, gps_longitude, read_odometer verileri Float32 tipinde yayınlanmaktadır.
+# /stm/steering_angle (INT8), /stm/motor_power (INT8), /stm/reset_encoder (Bool), /stm/brake (Bool) topiclerini abone olur ve veri geldiğinde araca iletir. 
 
-'''
- Usage example :
->>> import AKS_Communication as aks
->>> stm = aks.STM_Communication("COM10")
->>> stm.send_command(aks.Register.STEERING_ANGLE,20)
-
-'''
-
+import rospy
+from std_msgs.msg import Float32, Bool, Int8
 import minimalmodbus
-import time
 from enum import Enum
 
 class Register(Enum):
@@ -37,35 +31,80 @@ class Register(Enum):
 
 class STM_Communication:
     def __init__(self, port, slave_address=1, baudrate=38400):
-        self.instrument = minimalmodbus.Instrument(port, slave_address)
-        self.instrument.serial.baudrate = baudrate
-        # Port parametresine göre 'COMx' veya '/dev/ttyUSBx' gibi bir değer kullanabilirsiniz.
-        # slave_address, STM mikrodenetleyicinizin Modbus adresidir.
+        self.stm = minimalmodbus.Instrument(port, slave_address)
+        self.stm.serial.baudrate = baudrate
 
     def send_command(self, num_of_registers, data):
         try:
-            datatemp=data
-            if data>-32769 and data<32768:
-                if (data<0):
+            datatemp = data
+            if -32769 < data < 32768:
+                if data < 0:
                     data = 65536 + data
-                else:
-                    pass
-                self.instrument.write_register(num_of_registers.value, data, functioncode=6)
-                print(f'{num_of_registers.name} degeri {datatemp} olarak gonderildi.')
+                self.stm.write_register(num_of_registers.value, data, functioncode=6)
+                rospy.loginfo(f'{num_of_registers.name} degeri {datatemp} olarak gonderildi.')
             else:
-                print("Fonksiyon icerisine 32767 ila -32768 araliginda deger giriniz.")
-
-        except minimalmodbus.ModbusException as e:
-            print(f"hata : {e}")
+                rospy.logwarn("Fonksiyon icerisine 32767 ila -32768 araliginda deger giriniz.")
+        except Exception as e:
+            rospy.logwarn(f"AKS COMMUNICATION GONDERME HATASI: {e}")
             pass
 
-    def read_data(self,num_of_registers):
-        # Belirli bir register adresinden veri okuma
-        data = self.instrument.read_register(num_of_registers.value)
-        return data
+    def read_data(self, num_of_registers):
+        try:
+            data = self.stm.read_register(num_of_registers.value)
+            return data
+        except Exception as e:
+            rospy.logwarn(f"AKS COMMUNICATION OKUMA HATASI: {e}")
+            return None
+        
+    def steering_angle_callback(self, msg):
+        self.send_command(Register.STEERING_ANGLE, msg.data)
 
-if __name__ == "__main__":
+    def brake_callback(self, msg):
+        self.send_command(Register.BRAKE, msg.data)
 
-    stm=STM_Communication('COM10')
-    stm.send_command(Register.BRAKE,1)
-    print(stm.read_data(Register.READ_BRAKE_PRESSED))
+    def motor_power_callback(self, msg):
+        self.send_command(Register.MOTOR_POWER, msg.data)
+
+    def reset_encoder_callback(self, msg):
+        self.send_command(Register.RESET_ENCODER, msg.data)
+
+    def publish_data(self):
+        gps_latitude = self.read_data(self.stm, Register.GPS_LATITUDE)
+        gps_longitude = self.read_data(self.stm, Register.GPS_LONGITUDE)
+        read_odometer = self.read_data(self.stm, Register.READ_ODOMETER)
+        if gps_latitude is not None:
+            self.gps_latitude_pub.publish(gps_latitude)
+        if gps_longitude is not None:
+            self.gps_longitude_pub.publish(gps_longitude)
+        if read_odometer is not None:
+            self.read_odometer_pub.publish(read_odometer)
+
+        # Publishers
+        self.gps_latitude_pub = rospy.Publisher('gps_latitude', Float32, queue_size=10)
+        self.gps_longitude_pub = rospy.Publisher('gps_longitude', Float32, queue_size=10)
+        self.read_odometer_pub = rospy.Publisher('read_odometer', Float32, queue_size=10)
+
+        # Subscribers
+        rospy.Subscriber('/stm/steering_angle', Int8, self.steering_angle_callback)
+        rospy.Subscriber('/stm/motor_power', Int8, self.motor_power_callback)
+        rospy.Subscriber('/stm/reset_encoder', Bool, self.reset_encoder_callback)
+        rospy.Subscriber('/stm/brake', Bool, self.brake_callback)
+
+    def spin(self):
+        rate = rospy.Rate(10)  # 10 Hz
+        while not rospy.is_shutdown():
+            self.publish_data()
+            rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('stm32_node')
+        
+        # Veriables
+        port = '/dev/ttyUSB0' 
+        stm_node = STM_Communication(port)
+        
+        stm_node.spin()
+    except rospy.ROSInterruptException as e:
+        rospy.logwarn(f"AKS COMMUNICATION HATASI: {e}")
+        pass
