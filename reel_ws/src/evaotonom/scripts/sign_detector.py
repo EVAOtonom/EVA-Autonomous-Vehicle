382c00c14fcfbc7d1bbc7252d5fc84a5dae28956
Faruq2458 <150476215+Faruq2458@users.noreply.github.com>
2024-08-05 11:02:13 +0300

    şerit takibi bekleme eklendi

--- reel_ws/src/evaotonom/scripts/sign_detector.py
+++ reel_ws/src/evaotonom/scripts/sign_detector.py
@@ -253,5 +253,8 @@ def listener():
     rospy.spin()
 
 if __name__ == '__main__':
+    rospy.loginfo("Waiting for 'lane_track_node' service...")
+    rospy.wait_for_message("/lane_track/current_lane",Int8,timeout=100) # Şerit takibinin mevcut şerit bilgisini göndermesini bekler.
+    rospy.loginfo("'lane_track_node' service is now available.")
     listener()
 
