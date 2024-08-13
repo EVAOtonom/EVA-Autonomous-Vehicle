b5f4eb7897fe43f43322a5a51a0cc79ff362030f
Faruq2458 <150476215+Faruq2458@users.noreply.github.com>
2024-08-13 16:02:11 +0300

    Update decision_algorithm.py

--- reel_ws/src/evaotonom/scripts/decision_algorithm.py
+++ reel_ws/src/evaotonom/scripts/decision_algorithm.py
@@ -295,7 +295,7 @@ if __name__ == "__main__":
                     detection_control.publish(True)
                     reset_odom.publish(1)
                     time.sleep(0.5)
-                    while distance < depth -2:
+                    while distance < depth +1:
                         print(distance)
                     print("dist bitti")
                     time.sleep(0.5)
@@ -320,7 +320,7 @@ if __name__ == "__main__":
                     detection_control.publish(True)
                     reset_odom.publish(1)
                     time.sleep(0.5)
-                    while distance < depth -2:
+                    while distance < depth +1:
                         print(distance)
                     print("dist bitti")
                     time.sleep(0.5)
@@ -349,7 +349,7 @@ if __name__ == "__main__":
                     detection_control.publish(True)
                     reset_odom.publish(1)
                     time.sleep(0.5)
-                    while distance < depth -2:
+                    while distance < depth +1:
                         print(distance)
                     print("dist bitti")
                     time.sleep(0.5)                  
@@ -374,7 +374,7 @@ if __name__ == "__main__":
                     detection_control.publish(True)
                     reset_odom.publish(1)
                     time.sleep(0.5)
-                    while distance < depth -2:
+                    while distance < depth +1:
                         print(distance)
                     print("dist bitti")
                     time.sleep(0.5)
