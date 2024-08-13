5fa4d926b18165e1104c52d8697ac7496b40214e
Faruq2458 <150476215+Faruq2458@users.noreply.github.com>
2024-08-13 09:25:44 +0300

    Add files via upload

---
+++ reel_ws/src/evaotonom/scripts/gpsdegerleri.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.9
+
+import rospy
+from std_msgs.msg import Float32
+
+
+def latitude_callback(msg):
+    global latitude
+    latitude = msg.data
+
+def longitude_callback(msg):
+    global longitude
+    longitude = msg.data
+
+if __name__ == "__main__":
+    rospy.init_node('gps_coordinates', anonymous=True)
+
+    # Variables
+    latitude = None
+    longitude = None
+
+    # Subscribers
+    rospy.Subscriber('/stm/gps_latitude', Float32, latitude_callback)
+    rospy.Subscriber('/stm/gps_longitude', Float32, longitude_callback)
+
+    rate = rospy.Rate(1)  # 1 Hz
+
+    while not rospy.is_shutdown():
+        if latitude is not None and longitude is not None:
+            with open("coordinats.txt", "a") as file:
+                file.write(f"({latitude}, {longitude}),\n")
+        else:
+            rospy.logwarn("Veri alınamıyor")
+
+        rate.sleep()
